4e96160eebe1104986d68a380f427fad854e2b4b
jpschew <jps.chew@gmail.com>
2022-03-31 10:59:36 +0800

    added code and go.mod

--- main.go
+++ main.go
@@ -2,4 +2,16 @@ package main
 
 func main() {
 	//Create objects
+	game1 := game{"Minecraft", 5}
+	game2 := game{"World of Warcraft", 19}
+	game3 := game{"Elite Dangerours", 54}
+
+	var gameList list
+	gameList = append(gameList, game1, game2, game3)
+	// fmt.Println(list)
+	gameList.printContent()
+
+	for _, item := range gameList {
+		item.printPrice()
+	}
 }
